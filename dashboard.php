e760f8923a6418cc8c51a064cbae28638da9b451
Anthony Onyejesi <anthonyonyejesi@Anthonys-MacBook-Pro.local>
2020-04-09 23:59:09 +0100

    we move

--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,12 @@
 <?php include_once('lib/header.php');
 session_start();?>
-<p><?php echo "Welcome"." ".$_SESSION['userf']." ".$_SESSION['userl'];?></p>
+<p><?php echo "Welcome "." ".$_SESSION['userf']." ".$_SESSION['userl'];?></p>
 
+<p>first name :<?php echo $_SESSION['userf']?></p>
+<p>last name :<?php echo $_SESSION['userl']?></p>
 
 <p>designation: <?php echo $_SESSION['des']?></p>
 <p>gender: <?php echo $_SESSION['gender']?></p>
-<p>designation: <?php echo $_SESSION['des']?></p>
 
 <a href="control/proc.logout.php">Log out</a>
 <?php include_once('lib/footer.php');?>
